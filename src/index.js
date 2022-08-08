cd280bf84414ade8843285c0edd0ce7ccf5861b9
xElon-Mask <xelonmask@protonmail.com>
2022-08-08 12:24:18 +0200

    add authorText and avatar props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,9 @@ import CommentDetail from './CommentDetail';
 const App = () => {
     return (
         <div className='ui container comments'>
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" />
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" />
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" />
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" authorText="Super la brocante mon cousin !" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" authorText="Est-ce que c'est toi John Wayne, ou est-ce que c'est moi ?" avatar={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" authorText="HADRIIIENNNE !" avatar={faker.image.avatar()}/>
         </div>
     );
 };
