9d7b79a71c31755786f46fc3d34d81e606fa89d6
xElon-Mask <xelonmask@protonmail.com>
2022-08-19 13:01:16 +0200

    modified style.css and add component ApprovalCard

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import faker from 'faker';
-import CommentDetail from './CommentDetail';
+import "./style/App.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import faker from "faker";
+import CommentDetail from "./CommentDetail";
+import ApprovalCard from "./ApprovalCard";
 
 const App = () => {
-    return (
-        <div className='ui container comments'>
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" authorText="Super la brocante mon cousin !" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" authorText="Est-ce que c'est toi John Wayne, ou est-ce que c'est moi ?" avatar={faker.image.avatar()}/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" authorText="HADRIIIENNNE !" avatar={faker.image.avatar()}/>
-        </div>
-    );
+  return (
+    <div className="ui container comments">
+      <ApprovalCard>
+        <CommentDetail
+          author="Sam"
+          timeAgo="Today at 4:45PM"
+          authorText="Super la brocante mon cousin !"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Alex"
+          timeAgo="Today at 2:00AM"
+          authorText="Est-ce que c'est toi John Wayne, ou est-ce que c'est moi ?"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author="Jane"
+          timeAgo="Yesterday at 5:00PM"
+          authorText="HADRIIIENNNE !"
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+    </div>
+  );
 };
 
-
-ReactDOM.render(<App />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector("#root"));
